707ab76d9c3591f7730401302a6d35f387186ce8
Your Nameartomzak <kasabianstar@mail.ru>
2022-06-06 23:13:22 +0300

    JS-1:feature

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './scss/index.scss'
 import './scss/slider.scss'
+const importAll = (r) => r.keys().map(r)
+const images = importAll(
+  require.context('./img/', true, /.(?:png|jpeg|jpg|svg)$/i)
+)
+
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,1 +1,21 @@
-console.log("123");
+
+let position = 0;
+const slidesToShow = 3;
+const slidesToScroll = 2;
+const container = document.querySelector('.slider-container');
+const track =  document.querySelector('.slider-track');  
+const item = document.querySelector('.slider-item');  
+const btnPrev = document.querySelector('.btn-prev');  
+const btnNext =  document.querySelector('.btn-next');  
+const itemWidth = container.clientWidth / slidesToShow;
+
+// item.each(function (index, item){
+//   $(item).css({
+//     minWidth: itemWidth,
+//   });
+// })
+
+item.forEach((item) => {
+  item.style.minWidth = `${itemWidth}px`;    
+});
+
