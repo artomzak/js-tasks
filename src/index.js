a3e11c43cb93ae3ce7abb312b5f120dcb1011452
Your Nameartomzak <kasabianstar@mail.ru>
2022-06-20 20:28:01 +0300

    JS-1 feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './scss/index.scss'
 import './scss/slider.scss'
+import './js/index';
 const importAll = (r) => r.keys().map(r)
 const images = importAll(
   require.context('./img/', true, /.(?:png|jpeg|jpg|svg)$/i)
